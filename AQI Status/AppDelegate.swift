584ceafe14cb7bbb1f1761f47189ad9b32188c2d
John Abendroth <jaben3421@gmail.com>
2020-09-15 03:13:16 -0700

    Initial Commit

---
+++ AQI Status/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AQI Status
+//
+//  Created by John Abendroth on 9/15/20.
+//  Copyright © 2020 John Abendroth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AQI Status/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AQI Status
+//
+//  Created by John Abendroth on 9/15/20.
+//  Copyright © 2020 John Abendroth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
